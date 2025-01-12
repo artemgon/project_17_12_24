8cf0ae7aea11ac7aade7954b8447c828f7c1684e
artemgon <artemhoncharuk123@gmail.com>
2025-01-12 21:17:28 +0200

    Task 1 is done.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using project_17_12_24.Classes;
+using System.Reflection.Metadata.Ecma335;
+using System.Transactions;
 
 namespace project_17_12_24
 {
@@ -9,11 +11,49 @@ namespace project_17_12_24
             Console.ForegroundColor = ConsoleColor.DarkRed;
             try
             {
+                Action<string> GetRGB = delegate (string color)
+                {
+                    color = Lowercase(color);
+                    switch (color)
+                    {
+                        case "red":
+                            Console.WriteLine("Red: 255, 0, 0");
+                            break;
+                        case "orange":
+                            Console.WriteLine("Orange: 255, 165, 0");
+                            break;
+                        case "yellow":
+                            Console.WriteLine("Yellow: 255, 255, 0");
+                            break;
+                        case "green":
+                            Console.WriteLine("Green: 0, 128, 0");
+                            break;
+                        case "blue":
+                            Console.WriteLine("Blue: 0, 0, 255");
+                            break;
+                        case "indigo":
+                            Console.WriteLine("Indigo: 75, 0, 130");
+                            break;
+                        case "violet":
+                            Console.WriteLine("Violet: 238, 130, 238");
+                            break;
+                        default:
+                            Console.WriteLine("Invalid color");
+                            break;
+                    }
+                };
+                Console.WriteLine("Enter a color: ");
+                string color = Console.ReadLine() ?? "";
+                GetRGB(color);
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
             }
+        }
+        public static string Lowercase(string str)
+        {
+            return str.ToLower();
         }
     }
 }
