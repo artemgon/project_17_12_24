3f80dc644a38f06a6014e4a6082a859d21b47f74
artemgon <artemhoncharuk123@gmail.com>
2025-01-18 12:41:46 +0200

    Task 2 is done.

--- Program.cs
+++ Program.cs
@@ -9,6 +9,21 @@ namespace project_17_12_24
             Console.ForegroundColor = ConsoleColor.DarkRed;
             try
             {
+                List<string> content = [];
+                Rucksack rucksack = new("Black", "Nike", "Cotton", 0.5f, 10, content);
+                rucksack.AddContent("T-shirt", 2); 
+                Console.WriteLine();
+                rucksack.AddContent("Jeans", 3);   
+                Console.WriteLine();
+                rucksack.AddContent("Socks", 1);  
+                Console.WriteLine();
+                rucksack.AddContent("Jacket", 4);
+                Console.WriteLine();
+                rucksack.RemoveContent("Socks", 1);
+                Console.WriteLine();
+                rucksack.AddContent("Shoes", 4);
+                Console.WriteLine();
+                rucksack.Display(); 
             }
             catch (Exception e)
             {
