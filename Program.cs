61a945df490fe9bd8c59b24e0ddff70e1c6f5eca
artemgon <artemhoncharuk123@gmail.com>
2025-01-18 13:12:37 +0200

    Task 6 is done.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using project_17_12_24.Classes;
+using System.Reflection.Metadata;
 
 namespace project_17_12_24
 {
@@ -9,6 +10,26 @@ namespace project_17_12_24
             Console.ForegroundColor = ConsoleColor.DarkRed;
             try
             {
+                Console.WriteLine("Enter some text: ");
+                string text = Console.ReadLine() ?? "";
+                Console.WriteLine("Enter the word you want to find: ");
+                string word = Console.ReadLine() ?? "";
+                Func<string, string, string> checkWord = (txt, wrd) =>
+                {
+                    var words = txt.Split(' ');
+                    for (int i = 0; i < words.Length; i++)
+                    {
+                        if (words[i] == wrd)
+                        {
+                            string leftWord = i > 0 ? words[i - 1] : "No left word";
+                            string rightWord = i < words.Length - 1 ? words[i + 1] : "No right word";
+                            return $"Yeah, the word is found. Left word: {leftWord}, Right word: {rightWord}";
+                        }
+                    }
+                    return "No, the word is not found.";
+                };
+                string result = checkWord(text, word);
+                Console.WriteLine(result);
             }
             catch (Exception e)
             {
